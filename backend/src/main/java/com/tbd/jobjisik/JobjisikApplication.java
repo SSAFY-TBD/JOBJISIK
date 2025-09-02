5d7292476dbeba38c93db946e9145f6f0cea6ae3
Sngjuni <92257971+SngJuni@users.noreply.github.com>
2025-09-02 14:20:41 +0900

    🛠️ chore: 백엔드 초기 환경 세팅 (#2)

---
+++ backend/src/main/java/com/tbd/jobjisik/JobjisikApplication.java
@@ -0,0 +1,13 @@
+package com.tbd.jobjisik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobjisikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobjisikApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/tbd/jobjisik/JobjisikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tbd.jobjisik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobjisikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
